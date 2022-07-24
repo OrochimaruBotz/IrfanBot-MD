70be22560af10c015874c2516e3282ee45507f8e
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-07-24 10:07:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -25,11 +25,11 @@ const fs = require("fs")
 const chalk = require("chalk")
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
-global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoReadAll = true // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //documents variants
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
+global.owner = ['6288804947282'] //ur owner number
+global.ownername = "Yamato"
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "GitHub: OrochimaruBotz" //ur github or insta name
+global.location = "Indonesia, Jawa Timur, Surabaya" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
+global.botname = "YamatoBot" //ur bot name
 global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
-global.themeemoji = "🐶"
-global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
+global.packname = "YamatoBot\n\n\n\n\n\n\ "
+global.author = "Yamato"
+global.themeemoji = "🤖"
+global.reactmoji = "🕒"
+global.ownertag = ['6288804947282'] //ur owner tag
+global.ownernummenu = ['6288804947282'] //ur owner number in menu and all
+global.watermark = "YamatoBotz." //ur watermark
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.linkz1 = "https://chat.whatsapp.com/KZJwR34vNsK134HcFo8fxv"
+global.linkz2 = "https://chat.whatsapp.com/HI3tPSjQa6BFF5CjsmpZh5"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -95,18 +95,18 @@ global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.mess = {
     success: 'Done✓',
-    admin: 'This feature is only for admins!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This feature is only for owner',
-    group: 'This feature is only for groups!',
-    private: 'This feature is only for private chats!',
-    bot: 'This feature is only for bot',
+    admin: 'Perintah Hanya Bisa Digunakan Oleh Admin!',
+    botAdmin: 'Bot Bukan Seorang Admin!',
+    owner: 'Fitur Hanya Bisa Digunakan Oleh owner',
+    group: 'Fitur Hanya Bisa Digunakan Di Grup!',
+    private: 'Perintah Hanya Bisa Digunakan Di Chat Pribadi!',
+    bot: 'Perintah Hanya Bisa Digunakan Oleh Bot',
     wait: 'In process...',
-    linkm: 'Where is the link?',
+    linkm: 'Mana Linknya 🗿',
     error: 'Error!',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    nsfw: 'Fitur nsfw belum diaktifkan, silahkan hubungi admin untuk aktivasi',
+    banChat: 'Bot diblokir di grup ini, harap hubungi Owner untuk membatalkan pemblokiran'
 }
 global.limitawal = {
     premium: "Infinity",
